33b822f332d4c4d299ed99f99bb5b77847eb0a39
DavDevDav <bischof.david.db@gmail.com>
2024-07-12 10:32:03 +0200

    added the booking controller while making my classes better

--- code/AbschlussM223/src/main/java/com/example/abschlussm223/repo/StatusRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.abschlussm223.repo;
-
-import com.example.abschlussm223.models.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Long> {}
